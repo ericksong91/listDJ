08b0f9e31ecd5f6c74a7964da01ac4e8e223da77
Eric Song <ericksong91@gmail.com>
2023-07-24 17:47:03 -0400

    Adjusted session controller with private params and new error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
+    before_action :authorize, only: :destroy
 
     def create
         user = User.find_by(username: params[:username])
 
         if user&.authenticate(params[:password])
-            session[:user_id] = user.id
+            login_user
             render json: user
         else
             render json: { errors: "Invalid username or password" }, status: :unauthorized
@@ -12,14 +13,18 @@ class SessionsController < ApplicationController
     end
 
     def destroy
-        user = User.find_by(id: session[:user_id])
-        
-        if user
-            session.delete :user_id
-            head :no_content
-        else
-            render json: { errors: "Not authorized" }, status: :unauthorized
-        end
+        session.delete :user_id
+        head :no_content
+    end
+
+    private 
+
+    def login_user
+        session[:user_id] = user.id
+    end
+
+    def authorize
+        render json: { errors: "Not authorized" }, status: :unauthorized unless session[:user_id]
     end
 
 
