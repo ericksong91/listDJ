16eb162115f18e6d23df6e3b5e39bec53ea46cf9
Eric Song <ericksong91@gmail.com>
2023-08-08 19:25:15 -0400

    Sorted tracks by track_order from setlist_tracks

--- client/src/components/pages/SetlistPage.js
+++ client/src/components/pages/SetlistPage.js
@@ -1,27 +1,43 @@
 import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import TrackCard from "../cards/TrackCard";
 // import { useContext } from "react";
 // import { UserContext } from "../context/user";
 
 function SetlistPage( { setlists } ) {
     const index = parseInt(useParams().id);
-    const navigate = useNavigate();
-
-    console.log(setlists);
 
     if (!setlists) {
+        return <div></div>
+    };
 
-    }
+    const setFiltered = setlists.find(set => set.id === index);
 
-    // const tracksList = tracks.map((track) => {
-    //     return <TrackCard track={track} />
-    // })
+    // When updating by adding or removing songs, I should ONLY be updating the local cache of the set before "Save" 
+    // is hit by the user. To first organize the list, I should grab the array that represents the track order (setlist_tracks)
+    // After getting setlist_tracks with track order, I compare that to the array with all the tracks currently used.
+    // How to deal with tracks that are the same ID?
+
+    const tracks = setFiltered.tracks
+
+    const setlistTracks = setFiltered.setlist_track_org
+
+    const filteredList = [];
+
+    setlistTracks.forEach((slTrack) => {
+        filteredList.push(tracks.find((track) => track.id === slTrack.track_id))
+    });
+
+    const tracksList = filteredList.map((track) => <TrackCard key={track.id} track={track} />)
+
+    // const testSort = setlistTracks.sort((a, b) => setlistTracks.indexOf(a.track_order) - setlistTracks.indexOf(b.track_order))
+
+    // console.log(testSort)
 
     return (
         <div className="SetlistPage">
             <h1>Setlist Page</h1>
-            {/* {tracksList} */}
+            {tracksList}
         </div>
     );
 }
