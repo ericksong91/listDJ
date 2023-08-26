154026666b198010e717f71903b7db3c1bc89ee0
Eric Song <ericksong91@gmail.com>
2023-08-25 20:09:13 -0400

    Added strong params to setlist_tracks

--- app/controllers/setlist_tracks_controller.rb
+++ app/controllers/setlist_tracks_controller.rb
@@ -1,8 +1,16 @@
 class SetlistTracksController < ApplicationController
     def update
-        params.permit!
-        grouped_tracks = params[:_json].index_by{ |tr| tr[:id]}
+        grouped_tracks = tracklist_params[:_json].index_by{ |tr| tr[:id]}
         updated = SetlistTrack.update(grouped_tracks.keys, grouped_tracks.values)
         render json: updated, status: :accepted
     end
+
+
+    private
+
+    def tracklist_params
+        params.permit(:_json => [:id, :setlist_id, :track_id, :track_order])
+    end
+
+
 end
